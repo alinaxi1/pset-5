34a506797d0b421393288daddae54df93e9b4319
Alina Xi <AXi@ucvts.tec.nj.us>
2019-12-09 10:49:45 -0500

    cry

--- js/shapes.js
+++ js/shapes.js
@@ -21,6 +21,9 @@ window.onload = function() {
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
     document.getElementById("color").onclick = drawColoredRectangle;
+    document.getElementById("triangle").onclick = drawTriangle;
+    document.getElementById("smile").onclick = drawFace;
+    document.getElementById("pyramid").onclick = drawPyramid;
 }
 
  //Exercise 1.
@@ -40,7 +43,7 @@ const sayHello = function() {
   } while (message.length > 50);
 
   ctx.font = '48px sans-serif';
-  ctx.strokeText(message, 30, 70);
+  ctx.strokeText(message, 30, 70, 994);
 };
 
 //Exercise 2
@@ -51,24 +54,39 @@ const drawRectangle = function() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   do {
+
     var width = prompt("Width: ");
     var height = prompt("Height: ");
     var x = prompt("X-coordinate: ");
     var y = prompt("Y-coordinate: ");
 
     if (width > 1024 || width < 1) {
+        if (width == null) {
+          break;
+        }
         alert("Your width must be between 1 and 1024.");
       } else if (height > 512 || height < 1) {
+        if (height == null) {
+           break;
+         }
         alert("Your height must be between 1 and 512.");
       } else if (x > 1024 || x < 1) {
+        if (x == null) {
+           break;
+        }
         alert("Your x-coordinate must be between 1 and 1024.");
-      } else if (y > 512 || x < 1) {
+      } else if (y > 512 || y < 1) {
+        if (y == null) {
+          break;
+        }
         alert("Your y-coordinate must be between 1 and 512.");
-      } else {
+      } else if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)){
         alert("One of your values is not a number.");
+      } else if (x + width > 1024 || y + height > 512) {
+        alert("Your rectangle won't fit on canvas.")
       }
 
-  } while(width < 1 || width > canvas.width || height < 1 || height > canvas.height || x < 1 || y < 1);
+  } while(width < 1 || width > 1024 || height < 1 || height > 512 || x < 1 || y < 1 || x > 1024 || y > 512 || isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y) || x + width > 1024 || y + height > 512);
 
   ctx.strokeRect(x, y, width, height);
 };
@@ -82,17 +100,16 @@ const drawColoredRectangle = function() {
   const ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  do {
-    var color = String(prompt("Color: ")).toUpperCase();
+  let color = prompt("Color: ");
+  color = color.toLowerCase().trim();
 
-    if (color != "BLACK" && color != "BLUE" && color != "GREEN" && color != "ORANGE" && color != "PURPLE" && color != "RED" && color != "YELLOW") {
-      alert(color + " is not a supported color.");
-    }
-
-  } while (color != "BLACK" && color != "BLUE" && color != "GREEN" && color != "ORANGE" && color != "PURPLE" && color != "RED" && color != "YELLOW");
+  while (color != "black" && color != "blue" && color != "green" && color != "orange" && color != "purple" && color != "red" && color != "yellow") {
+    alert(color + " is not a supported color.");
+    color = prompt("Color: ");
+  }
 
   ctx.fillStyle = color;
-  ctx.filledRect(10, 10, 100, 50);
+  ctx.fillRect(10, 10, 100, 50);
 };
 
 /*
@@ -100,7 +117,25 @@ const drawColoredRectangle = function() {
  */
 
 const drawTriangle = function() {
-    // write your exercise 4 code here
+  const canvas = document.getElementById('student-canvas-4');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  let height = prompt("Height: ");
+  let hypotenuse = prompt("Hypotenuse: ");
+  let base = prompt("Base: ");
+
+  while (((height * height) != (hypotenuse * hypotenuse) + (base * base)) || height > 512 || height < 1 || base > 1024 || base < 1 || hypotenuse < 1) {
+    alert("That's not a valid right triangle.");
+  }
+
+ctx.beginPath();
+ctx.moveTo(25, 25);
+ctx.lineTo(25, 25 + height / 10);
+ctx.lineTo(25 + base, 25 + height);
+ctx.closePath();
+ctx.stroke();
+
 };
 
 /*
@@ -122,14 +157,20 @@ const drawPyramid = function() {
 
   do {
     var block = prompt("Side: ");
+
     if (block < 1) {
       alert("Your block size must be at least 1.");
     } else if (block > canvas.height || block > canvas.width){
       alert("Your pyramid won't fit on canvas.");
-    } else {
+    } else if (isNaN(block)){
       alert("Your block size is not a number.")
     }
-    
+
+    for (var k = 0; k <= 5; k++){
+      ctx.strokeRect(10, 10, block, block);
+    }
+
+
   } while (block < 1 || block > canvas.height || block > canvas.width || isNaN(block));
 
 };
